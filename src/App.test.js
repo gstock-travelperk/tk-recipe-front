4f7aba9b141c0b379956f1a46e28cd37c6b95c23
Gabriel Stock <gabriel.stock@travelperk.com>
2020-10-09 15:35:19 -0300

    Unified tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,10 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("test renders TK Recipe App", () => {
+test("test renders TK Recipe App", async () => {
   render(<App />);
   const linkElement = screen.getByText(/TK Recipe App/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test("access recipies", async () => {
-  render(<App />);
   userEvent.click(screen.getByRole("link"));
   expect(
     await screen.findByText(/This is the list of recipes/i)
