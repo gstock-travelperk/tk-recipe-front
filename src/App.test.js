168f628ad30d12d239adf8bba8e6e3b1caf44657
Gabriel Stock <gabriel.stock@travelperk.com>
2020-10-09 13:42:22 -0300

    Use screen on test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("test renders TK Recipe App", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TK Recipe App/i);
+  render(<App />);
+  const linkElement = screen.getByText(/TK Recipe App/i);
   expect(linkElement).toBeInTheDocument();
 });
